4f3675e7ac2cf9bb39815ebc30470f107d5e18ca
jalloulabdo <jalloulabdo88@gmail.com>
2024-01-26 20:17:13 +0100

    change icon link

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,7 @@ import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
+import link from "./link.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
@@ -66,4 +67,5 @@ export {
   carTrips,
   ultras,
   eurostyle,
+  link,
 };
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,7 +3,7 @@ import { Tilt } from 'react-tilt'
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { github } from "../assets";
+import { link } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
@@ -37,10 +37,10 @@ const ProjectCard = ({
           <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
             <div
               onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+              className='gradient-violet w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
             >
               <img
-                src={github}
+                src={link}
                 alt='source code'
                 className='w-1/2 h-1/2 object-contain'
               />
